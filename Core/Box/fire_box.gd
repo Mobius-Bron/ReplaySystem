54847fe0acfa6de9752a807edc745233429bd0b1
Amber Ionian <156796273+Mobius-Bron@users.noreply.github.com>
2026-04-22 02:17:13 +0800

    添加录制重播类

--- Core/Box/fire_box.gd
+++ Core/Box/fire_box.gd
@@ -8,4 +8,5 @@ func _on_fire_box_area_area_entered(area: Area3D) -> void:
 		var new_bullet: Bullet = BULLET.instantiate()
 		
 		get_tree().root.add_child(new_bullet)
+		new_bullet.speed = randf_range(5.0, 15.0)
 		new_bullet.global_position = self.global_position + Vector3(randf_range(-1, 1), 0.0, randf_range(-1, 1))
